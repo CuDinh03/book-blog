832bfb17c52c059562a63cfbadc5d515f6a1fda7
CuDinh03 <dinhhuycu0305@gmail.com>
2024-11-11 23:47:20 +0700

    fix docker-compose boookblog

--- authen-service/src/main/java/book/blog/authenservice/AuthenServiceApplication.java
+++ authen-service/src/main/java/book/blog/authenservice/AuthenServiceApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuthenServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthenServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenServiceApplication.class, args);
+        System.out.println("ok authen");
+    }
 
 }
